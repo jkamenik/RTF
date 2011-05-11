6f3f548f08efe3643c831473662fa80ffaaf7394
John Kamenik <jkamenik@gmail.com>
2011-05-11 15:55:28 -0400

    Repo model mostly works.

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -1,3 +1,5 @@
+require 'fileutils'
+
 class Repo < ActiveRecord::Base
   def git_create
     g = repo rescue Git.clone(repo_path, git_path)
@@ -5,16 +7,18 @@ class Repo < ActiveRecord::Base
   end
   
   def git_update
-    repo.pull
+    r = repo
+    r.pull
+    Commit.find_or_create r.gcommit('HEAD')
   end
   
   def recalculate
     git_update
-    parse_output run_commands
+    parse_output *run_commands
   end
   
   def git_cleanup
-    
+    FileUtils.rm_rf git_path if File.exists? git_path
   end
   
 private
@@ -29,8 +33,37 @@ private
   end
   
   def run_commands
+    commands = setup_command.split("\n")
+    commands.push cucumber_comand
+    
+    orig_ENV = ENV.to_hash
+    ENV.clear
+    capture do
+      system(commands.join(';'))
+      $?.exitstatus
+    end
+  ensure
+    ENV.clear
+    orig_ENV.to_hash.each{ |k, v| ENV[k] = v}
   end
   
-  def parse_output(output)
+  def parse_output(status,output,error)
+  end
+  
+  def capture(&block)
+    begin
+      out = $stdout
+      err = $stderr
+      $stdout = StringIO.new
+      $stderr = StringIO.new
+      status = yield
+      output = $stdout.string
+      error  = $stderr.string
+    ensure
+      $stdout = out
+      $stderr = err
+    end
+    
+    [status,output,error]
   end
 end
--- spec/models/repo_spec.rb
+++ spec/models/repo_spec.rb
@@ -3,8 +3,9 @@ require 'spec_helper'
 describe Repo do
   before :all do
     class Repo
-      public :repo, :run_commands, :parse_output
+      public :repo, :run_commands, :parse_output, :capture
     end
+    system('echo test >/dev/null') #give us $?
   end
   
   context 'git_create' do
@@ -37,27 +38,99 @@ describe Repo do
     end
   end
   
+  context 'git_cleanup' do
+    it 'should do nothing if file does not exist' do
+      FileUtils.should_not_receive(:rm_rf).with('foo')
+      File.should_receive(:exists?).with('foo').and_return false
+      x = Repo.new
+      x.stub(:git_path).and_return 'foo'
+      x.git_cleanup
+    end
+    
+    it 'should rm -rf if file does exist' do
+      FileUtils.should_receive(:rm_rf).with('foo').and_return []
+      File.should_receive(:exists?).with('foo').and_return true
+      x = Repo.new
+      x.stub(:git_path).and_return 'foo'
+      x.git_cleanup
+    end
+  end
+  
   context 'recalculate' do
     it 'should update, run commands, and parse output' do
       x = Repo.new
       x.should_receive :git_update
-      x.should_receive :run_commands
-      x.should_receive :parse_output
+      x.should_receive(:run_commands).and_return [1,2,3]
+      x.should_receive(:parse_output).with(1,2,3)
       x.recalculate
     end
   end
   
   context 'run_commands' do
-    it 'should shell out'
+    it 'should shell out setup_command; cucumber_command' do
+      x = Repo.new
+      x.stub!(:setup_command).and_return 'echo foo'
+      x.stub!(:cucumber_comand).and_return "echo bar"
+      x.should_receive(:system).with("echo foo;echo bar").and_return ''
+      x.run_commands
+    end
     
-    it 'should clean the environment'
+    it 'should clean the environment' do
+      ENV['TEST'] = 'foo'
+      x = Repo.new
+      x.stub!(:setup_command).and_return 'export FOO=1'
+      x.stub!(:cucumber_comand).and_return "echo $TEST"
+      values = x.run_commands
+      values.class.should == Array
+      values[1].should == ''
+      ENV['TEST'].should == 'foo'
+      ENV['FOO'].should be_nil
+    end
     
-    it 'should collect the output'
-    
-    it 'should return the string output'
+    it 'should collect the output' do
+      x = Repo.new
+      x.stub!(:setup_command).and_return 'echo foo'
+      x.stub!(:cucumber_comand).and_return "echo bar >&2"
+      x.run_commands
+
+      values.class.should == Array
+      values[1].should == "foo\n"
+      values[2].should == "bar\n"
+    end
   end
   
   context 'parse_output' do
     pending "not sure yet"
+  end
+  
+  context 'capture' do
+    it 'should return 3 items, block return value, stdout, and stdin' do
+      x = Repo.new
+      values = x.capture {
+        puts 'out'
+        warn 'err'
+        4
+      }
+      values.class.should == Array
+      values[0].should == 4
+      values[1].should == "out\n"
+      values[2].should == "err\n"
+    end
+    
+    it 'should capture stdout' do
+      x = Repo.new
+      values = x.capture {
+        puts 'test'
+      }
+      values[1].should == "test\n"
+    end
+    
+    it 'should capture stderr' do
+      x = Repo.new
+      values = x.capture {
+        warn 'test'
+      }
+      values[2].should == "test\n"
+    end
   end
 end
